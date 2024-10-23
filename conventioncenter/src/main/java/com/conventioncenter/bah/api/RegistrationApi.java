b3023317a8199843ff17a1c1ec8d373c4f69f773
Winston Tan <tanwinston217@yahoo.com>
2024-10-23 19:54:28 +0000

    added registration

---
+++ conventioncenter/src/main/java/com/conventioncenter/bah/api/RegistrationApi.java
@@ -0,0 +1,29 @@
+package com.conventioncenter.bah.api;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.conventioncenter.bah.domain.Registration;
+import com.conventioncenter.bah.persistence.InMemoryRegistrationRepository;
+
+@RestController
+public class RegistrationApi {
+
+	
+	@GetMapping("/registration/id/{id}")
+	public Registration getCustomer(@PathVariable("id")Long id) {
+		System.out.println("ID = " + id);
+		InMemoryRegistrationRepository registration = new InMemoryRegistrationRepository();
+		return registration.findById(id);
+	}
+	
+	@GetMapping("/registration")
+	public List<Registration> getAllRegistrations(){
+		InMemoryRegistrationRepository registration = new InMemoryRegistrationRepository();
+		return registration.getAll();
+	}
+
+}
