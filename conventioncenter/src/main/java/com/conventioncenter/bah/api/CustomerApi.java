33f908d1087f2e899090ef17eea639bdd36e2bef
wasadmin <lnkrobath@gmail.com>
2024-10-23 19:53:26 +0000

    made events

--- conventioncenter/src/main/java/com/conventioncenter/bah/api/CustomerApi.java
+++ conventioncenter/src/main/java/com/conventioncenter/bah/api/CustomerApi.java
@@ -2,7 +2,6 @@ package com.conventioncenter.bah.api;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +11,15 @@ import com.conventioncenter.bah.persistence.InMemoryCustomerRepository;
 
 @RestController
 public class CustomerApi {
-	
-	InMemoryCustomerRepository customers;
-	
 	@GetMapping("/customers/id/{id}")
 	public Customer getCustomer(@PathVariable("id")Long id) {
-		System.out.println("ID = " + id);
-		customers = new InMemoryCustomerRepository();
+		InMemoryCustomerRepository customers = new InMemoryCustomerRepository();
 		return customers.findById(id);
 	}
 	
 	@GetMapping("/customers")
 	public List<Customer> getAllCustomers(){
+		InMemoryCustomerRepository customers = new InMemoryCustomerRepository();
 		return customers.getAll();
 	}
 
