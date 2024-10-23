5eea1a4d30158c6fcc7eac52c66751f8281c039c
wasadmin <lnkrobath@gmail.com>
2024-10-23 15:45:19 +0000

    added spring initializer

---
+++ conventioncenter/src/main/java/com/conventioncenter/bah/ConventioncenterApplication.java
@@ -0,0 +1,13 @@
+package com.conventioncenter.bah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConventioncenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConventioncenterApplication.class, args);
+	}
+
+}
