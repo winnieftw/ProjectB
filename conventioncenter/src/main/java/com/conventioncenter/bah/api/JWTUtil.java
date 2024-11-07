5eaa3f863a2503a095a1e0ee18f84593e0f0e2fd
wasadmin <lnkrobath@gmail.com>
2024-11-07 19:32:23 +0000

    Adding byname

---
+++ conventioncenter/src/main/java/com/conventioncenter/bah/api/JWTUtil.java
@@ -0,0 +1,8 @@
+package com.conventioncenter.bah.api;
+
+
+public interface JWTUtil {
+	public boolean verifyToken(String jwt_token);
+	public String getScopes(String jwt_token) ;
+	public Token createToken(String scopes) ;
+}
